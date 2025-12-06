3f7fcf307382472e503b02055dd5a75280f21e0b
MakdumIbrohim <makdumibrohim3@gmail.com>
2025-12-06 17:48:08 +0700

    style(ui): adjust layout and spacing in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import DatePicker from "../components/DateRangePicker";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-end w-[80%] h-auto bg-blue-500 text-white p-6 text-center absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="flex justify-center items-v w-[80%] h-auto bg-blue-500 text-white p-6 text-center absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3">
       <div className="text-left">
         <p className="mb-4">
           Select a date range to calculate the number of days between two dates.
